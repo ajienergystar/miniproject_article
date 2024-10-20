fcb705e024b1b49990b726f611bb46ed33874898
macAji <ajienergystar@gmail.com>
2024-10-20 15:25:03 +0800

    first create project

---
+++ miniproject_article/miniproject_article/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  miniproject_article
+//
+//  Created by mac on 20/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ miniproject_article/miniproject_article/miniproject_articleApp.swift
@@ -0,0 +1,17 @@
+//
+//  miniproject_articleApp.swift
+//  miniproject_article
+//
+//  Created by mac on 20/10/24.
+//
+
+import SwiftUI
+
+@main
+struct miniproject_articleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
